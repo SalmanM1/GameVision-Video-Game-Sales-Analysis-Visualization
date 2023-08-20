e634a6da500269e2c04db807611aaf7392aa16b9
SalmanM1 <139178645+SalmanM1@users.noreply.github.com>
2023-08-19 20:50:19 -0500

    Add files via upload

---
+++ DataAnalysis.py
@@ -0,0 +1,30 @@
+
+# Original code provided by the user goes here
+# ...
+
+# All the code provided earlier in the conversation
+
+
+
+# Exploratory Data Analysis
+
+# Loading Libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+# Load Data
+file_path = '/path/to/your/csv/file.csv'
+vg_sales_data = pd.read_csv(file_path)
+
+# Data Cleaning and Preprocessing
+# ...
+
+# Visualizations
+# ...
+
+# Conclusion
+# ...
+
